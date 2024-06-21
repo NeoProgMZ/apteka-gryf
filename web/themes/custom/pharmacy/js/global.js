95b87a6a44e19b4d2110aa017d1b1b90f37d29c3
DDEV User <nobody@example.com>
2024-06-21 19:05:22 +0000

    [WIP] szablon

--- web/themes/custom/pharmacy/js/global.js
+++ web/themes/custom/pharmacy/js/global.js
@@ -5,12 +5,36 @@
  */
 (function (Drupal) {
 
-  'use strict';
+    'use strict';
 
-  Drupal.behaviors.pharmacy = {
-    attach: function (context, settings) {
+    Drupal.behaviors.bootstrap_barrio = {
+        attach: function (context, settings) {
 
-    }
-  };
+            var position = window.scrollY;
+            window.addEventListener('scroll', function () {
+                if (window.scrollY > 50) {
+                    document.querySelector('body').classList.add("scrolled");
+                }
+                else {
+                    document.querySelector('body').classList.remove("scrolled");
+                }
+                var scroll = window.scrollY;
+                if (scroll > position) {
+                    document.querySelector('body').classList.add("scrolldown");
+                    document.querySelector('body').classList.remove("scrollup");
+                } else {
+                    document.querySelector('body').classList.add("scrollup");
+                    document.querySelector('body').classList.remove("scrolldown");
+                }
+                position = scroll;
+            });
+        }
+    };
+
+    Drupal.behaviors.pharmacy = {
+        attach: function (context, settings) {
+
+        }
+    };
 
 })(Drupal);
